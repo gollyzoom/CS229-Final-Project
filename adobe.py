a35f98b252cd13e4f1b2789bd3c6c637900c683b
gollyzoom <gollyzoom@gmail.com>
2023-11-21 15:46:12 -0800

    Update adobe.py
    improve variable names + code cleanup

--- adobe.py
+++ adobe.py
@@ -1,12 +1,9 @@
 # Import library from Image
 from wand.image import Image
 
-# Import the image
-
+# this function applies the Adobe watermark to an image specified by filename.
 def apply_adobe(image,fname):
-	with Image(filename ='watermarks/adobe.png') as water:
-		#print(water.size)
-		#print(image.size)
+	with Image(filename ='watermarks/adobe.png') as adobe_watermark:
 		crop_width = 0
 		crop_height = 0
 
@@ -15,18 +12,11 @@ def apply_adobe(image,fname):
 		else:
 			image.crop(0,0,image.size[1],image.size[1])
 		image.resize(256,256)
-		water.resize(int(image.size[0]*.6),int(water.size[1]*(.6*image.size[0]/water.size[0])))
+		adobe_watermark.resize(int(image.size[0]*.6),int(adobe_watermark.size[1]*(.6*image.size[0]/adobe_watermark.size[0])))
 		# Clone the image in order to process
-		with image.clone() as watermark:
+		with image.clone() as watermarked_image:
+			# apply the adobe watermark
+			watermarked_image.watermark(adobe_watermark,.5,int(.5*image.size[0] - .5*adobe_watermark.size[0]),int(.5*image.size[1] - .5*adobe_watermark.size[1]))
 
-			#turn on for shutterstock
-			#watermark.watermark(water,.4, int(image.size[0]*-.5),int(image.size[1]*-.5))
-
-			#turn on for 123rf
-			#watermark.watermark(water,.5, 0,0)
-
-			#turn on for adobe
-			watermark.watermark(water,.5,int(.5*image.size[0] - .5*water.size[0]),int(.5*image.size[1] - .5*water.size[1]))
-
-				# Save the image
-			watermark.save(filename =fname)
+			# Save the watermarked image
+			watermarked_image.save(filename=fname)
